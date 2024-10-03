60eba9ad3a906f70abedb69a79cd2c092c6b9f2e
Shashank <139771571+Shashankxh@users.noreply.github.com>
2024-10-03 10:27:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,38 @@ function convertToRoman(num) {
     };
 
   //your code here
+ let result = ''; // This will store the Roman numeral
 
+    // Array representing subtractive combinations for specific Roman numeral patterns
+    const subtractiveCombinations = [
+        ['CM', 900], // 900 is represented as CM
+        ['CD', 400], // 400 is represented as CD
+        ['XC', 90],  // 90 is represented as XC
+        ['XL', 40],  // 40 is represented as XL
+        ['IX', 9],   // 9 is represented as IX
+        ['IV', 4]    // 4 is represented as IV
+    ];
+
+    // Handle subtractive combinations first
+    for (let [symbol, value] of subtractiveCombinations) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    // Now handle the standard Roman numeral values in the obj object
+    for (let key in obj) {
+        const [symbol, value] = obj[key];
+
+        // Append symbols to the result while subtracting their value from num
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
