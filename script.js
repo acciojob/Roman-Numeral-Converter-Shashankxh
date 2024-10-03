caa0be8377819df1603a2e839b0f30d951863b1d
Shashank <139771571+Shashankxh@users.noreply.github.com>
2024-10-03 10:29:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-
-  //your code here
- let result = ''; // This will store the Roman numeral
-
-    // Array representing subtractive combinations for specific Roman numeral patterns
-    const subtractiveCombinations = [
-        ['CM', 900], // 900 is represented as CM
-        ['CD', 400], // 400 is represented as CD
-        ['XC', 90],  // 90 is represented as XC
-        ['XL', 40],  // 40 is represented as XL
-        ['IX', 9],   // 9 is represented as IX
-        ['IV', 4]    // 4 is represented as IV
+    // List of Roman numerals and their corresponding values in descending order
+    const romanNumerals = [
+        ['M', 1000],
+        ['CM', 900],   // Subtractive combination for 900
+        ['D', 500],
+        ['CD', 400],   // Subtractive combination for 400
+        ['C', 100],
+        ['XC', 90],    // Subtractive combination for 90
+        ['L', 50],
+        ['XL', 40],    // Subtractive combination for 40
+        ['X', 10],
+        ['IX', 9],     // Subtractive combination for 9
+        ['V', 5],
+        ['IV', 4],     // Subtractive combination for 4
+        ['I', 1]
     ];
 
-    // Handle subtractive combinations first
-    for (let [symbol, value] of subtractiveCombinations) {
-        while (num >= value) {
-            result += symbol;
-            num -= value;
-        }
-    }
+    let result = '';  // This will store the Roman numeral
 
-    // Now handle the standard Roman numeral values in the obj object
-    for (let key in obj) {
-        const [symbol, value] = obj[key];
-
+    // Iterate over each Roman numeral in the list
+    for (let [symbol, value] of romanNumerals) {
         // Append symbols to the result while subtracting their value from num
         while (num >= value) {
             result += symbol;
@@ -43,12 +29,9 @@ function convertToRoman(num) {
 
     return result;
 }
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
+console.log(convertToRoman(36));  // Output: XXXVI
 
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman;
